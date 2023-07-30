6e5b83b868cf7fde45b38fda7db42ae3c3ab1e48
Brandon Nicolas Marlim <itsbrandonlimm@gmail.com>
2023-07-30 09:42:02 +0700

    first commit

---
+++ IColab/IColabApp.swift
@@ -0,0 +1,17 @@
+//
+//  IColabApp.swift
+//  IColab
+//
+//  Created by Brandon Nicolas Marlim on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct IColabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
