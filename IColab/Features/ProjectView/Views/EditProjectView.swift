37a65fdd18787a94db709b53904f0f8a51ccfede
Raymond <metekohy233@gmail.com>
2023-09-26 16:00:55 +0700

    Added logic to CurrentTaskView and added EditProjectView

---
+++ IColab/Features/ProjectView/Views/EditProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProjectView.swift
+//  IColab
+//
+//  Created by Jeremy Raymond on 26/09/23.
+//
+
+import SwiftUI
+
+struct EditProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProjectView()
+}
