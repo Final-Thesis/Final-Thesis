17b16a9fd47129b2c1328418495435f742b40491
Raymond <metekohy233@gmail.com>
2023-10-03 11:08:17 +0700

    Implemented random project title on the MockData

--- IColab/Core/Data/Mock/MockData/MockAccounts/MockChats.swift
+++ IColab/Core/Data/Mock/MockData/MockAccounts/MockChats.swift
@@ -8,22 +8,24 @@
 import Foundation
 
 struct MockChats {
-    static var randomProjectTitle: String = MockProjects.projects.randomElement()!.title
-    
     static var chats: [Chat] = [
-        Chat(name: "John", messages: MockMessages.generateMessages(), type: .owner, projectName: randomProjectTitle),
-        Chat(name: "Doe", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Kevin", messages: MockMessages.generateMessages(), type: .owner, projectName: randomProjectTitle),
-        Chat(name: "Dallian", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Gregorius", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Jeremy", messages: MockMessages.generateMessages(), type: .owner, projectName: randomProjectTitle),
-        Chat(name: "Raymond", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Metekohy", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Brandon", messages: MockMessages.generateMessages(), type: .owner, projectName: randomProjectTitle),
-        Chat(name: "Nicholas", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Marlim", messages: MockMessages.generateMessages(), projectName: randomProjectTitle),
-        Chat(name: "Dishcovery", messages: MockMessages.generateMessages(), type: .group, projectName: randomProjectTitle),
-        Chat(name: "IColab", messages: MockMessages.generateMessages(), type: .group, projectName: randomProjectTitle),
-        Chat(name: "Slipi", messages: MockMessages.generateMessages(), type: .group, projectName: randomProjectTitle),
+        Chat(name: "John", messages: MockMessages.generateMessages(), type: .owner, projectName: randomizeProjectTitle()),
+        Chat(name: "Doe", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Kevin", messages: MockMessages.generateMessages(), type: .owner, projectName: randomizeProjectTitle()),
+        Chat(name: "Dallian", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Gregorius", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Jeremy", messages: MockMessages.generateMessages(), type: .owner, projectName: randomizeProjectTitle()),
+        Chat(name: "Raymond", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Metekohy", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Brandon", messages: MockMessages.generateMessages(), type: .owner, projectName: randomizeProjectTitle()),
+        Chat(name: "Nicholas", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Marlim", messages: MockMessages.generateMessages(), projectName: randomizeProjectTitle()),
+        Chat(name: "Dishcovery", messages: MockMessages.generateMessages(), type: .group, projectName: randomizeProjectTitle()),
+        Chat(name: "IColab", messages: MockMessages.generateMessages(), type: .group, projectName: randomizeProjectTitle()),
+        Chat(name: "Slipi", messages: MockMessages.generateMessages(), type: .group, projectName: randomizeProjectTitle()),
     ]
+    
+    static func randomizeProjectTitle() -> String {
+        return MockProjects.projects.randomElement()!.title
+    }
 }
