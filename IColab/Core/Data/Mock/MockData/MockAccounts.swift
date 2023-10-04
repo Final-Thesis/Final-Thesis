712c36f78bf7945d297f2b1ac8546aa60ab9cae6
Raymond <metekohy233@gmail.com>
2023-10-04 08:32:24 +0700

    Added more Accounts and Projects in MockData for testing purposes

--- IColab/Core/Data/Mock/MockData/MockAccounts.swift
+++ IColab/Core/Data/Mock/MockData/MockAccounts.swift
@@ -20,8 +20,28 @@ struct MockAccounts {
             accountDetail: MockAccountDetails.accountDetails[0]
         ),
         Account(
+            email: "dallian@gmail.com",
+            password: "dallian@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
+        Account(
             email: "gregorius@gmail.com",
             password: "gregorius@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
+        Account(
+            email: "jeremy@gmail.com",
+            password: "jeremy@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
+        Account(
+            email: "raymond@gmail.com",
+            password: "raymond@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
+        Account(
+            email: "metekohy@gmail.com",
+            password: "metekohy@123",
             accountDetail: MockAccountDetails.accountDetails[0]
         ),
         Account(
@@ -29,46 +49,56 @@ struct MockAccounts {
             password: "brandon@123",
             accountDetail: MockAccountDetails.accountDetails[0]
         ),
+        Account(
+            email: "nicholas@gmail.com",
+            password: "nicholas@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
+        Account(
+            email: "marlim@gmail.com",
+            password: "marlim@123",
+            accountDetail: MockAccountDetails.accountDetails[0]
+        ),
     ]
     
-    static func setProjects() {
-        MockAccounts.accounts = [
-            Account(
-                email: "john@gmail.com",
-                password: "john@123",
-                accountDetail: MockAccountDetails.accountDetails[0],
-                projectsOwned: Mock.projects,
-                projectsJoined: Mock.projects,
-                notifications: Mock.notifications,
-                chats: Mock.chats
-            ),
-            Account(
-                email: "kevin@gmail.com",
-                password: "kevin@123",
-                accountDetail: MockAccountDetails.accountDetails[0],
-                projectsOwned: Mock.projects,
-                projectsJoined: Mock.projects,
-                notifications: Mock.notifications,
-                chats: Mock.chats
-            ),
-            Account(
-                email: "gregorius@gmail.com",
-                password: "gregorius@123",
-                accountDetail: MockAccountDetails.accountDetails[0],
-                projectsOwned: Mock.projects,
-                projectsJoined: Mock.projects,
-                notifications: Mock.notifications,
-                chats: Mock.chats
-            ),
-            Account(
-                email: "brandon@gmail.com",
-                password: "brandon@123",
-                accountDetail: MockAccountDetails.accountDetails[0], 
-                projectsOwned: Mock.projects,
-                projectsJoined: Mock.projects,
-                notifications: Mock.notifications,
-                chats: Mock.chats
-            ),
-        ]
-    }
+//    static func setProjects() {
+//        MockAccounts.accounts = [
+//            Account(
+//                email: "john@gmail.com",
+//                password: "john@123",
+//                accountDetail: MockAccountDetails.accountDetails[0],
+//                projectsOwned: Mock.projects,
+//                projectsJoined: Mock.projects,
+//                notifications: Mock.notifications,
+//                chats: Mock.chats
+//            ),
+//            Account(
+//                email: "kevin@gmail.com",
+//                password: "kevin@123",
+//                accountDetail: MockAccountDetails.accountDetails[0],
+//                projectsOwned: Mock.projects,
+//                projectsJoined: Mock.projects,
+//                notifications: Mock.notifications,
+//                chats: Mock.chats
+//            ),
+//            Account(
+//                email: "gregorius@gmail.com",
+//                password: "gregorius@123",
+//                accountDetail: MockAccountDetails.accountDetails[0],
+//                projectsOwned: Mock.projects,
+//                projectsJoined: Mock.projects,
+//                notifications: Mock.notifications,
+//                chats: Mock.chats
+//            ),
+//            Account(
+//                email: "brandon@gmail.com",
+//                password: "brandon@123",
+//                accountDetail: MockAccountDetails.accountDetails[0], 
+//                projectsOwned: Mock.projects,
+//                projectsJoined: Mock.projects,
+//                notifications: Mock.notifications,
+//                chats: Mock.chats
+//            ),
+//        ]
+//    }
 }
--- IColab/Core/Data/Mock/MockData/MockProjects.swift
+++ IColab/Core/Data/Mock/MockData/MockProjects.swift
@@ -38,7 +38,7 @@ struct MockProjects {
             ]
         ),
         Project(
-            title: "Project Title",
+            title: "Vision Project",
             owner: Mock.accounts[0],
             role: "Back-end",
             requirements: ["3 years SwiftUI experience", "3 years UIKit experience"],
@@ -50,7 +50,63 @@ struct MockProjects {
                 MockMilestones.milestones[0],
                 MockMilestones.milestones[1]
             ]
-        )
+        ),
+        Project(
+            title: "Database Project",
+            owner: Mock.accounts[0],
+            role: "Front-end",
+            requirements: ["3 years SwiftUI experience", "3 years UIKit experience"],
+            tags: ["SwiftUI", "CoreML", "Vision"],
+            startDate: Date.now,
+            endDate: Date.now.addingTimeInterval(10000),
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            milestones: [
+                MockMilestones.milestones[0],
+                MockMilestones.milestones[1]
+            ]
+        ),
+        Project(
+            title: "Design Project",
+            owner: Mock.accounts[1],
+            role: "Back-end",
+            requirements: ["3 years SwiftUI experience", "3 years UIKit experience"],
+            tags: ["SwiftUI", "CoreML", "Vision"],
+            startDate: Date.now,
+            endDate: Date.now.addingTimeInterval(10),
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            milestones: [
+                MockMilestones.milestones[0],
+                MockMilestones.milestones[1]
+            ]
+        ),
+        Project(
+            title: "Cloud Project",
+            owner: Mock.accounts[0],
+            role: "Back-end",
+            requirements: ["3 years SwiftUI experience", "3 years UIKit experience"],
+            tags: ["SwiftUI", "CoreML", "Vision"],
+            startDate: Date.now,
+            endDate: Date.now.addingTimeInterval(10),
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            milestones: [
+                MockMilestones.milestones[0],
+                MockMilestones.milestones[1]
+            ]
+        ),
+        Project(
+            title: "IOT Project",
+            owner: Mock.accounts[0],
+            role: "Back-end",
+            requirements: ["3 years SwiftUI experience", "3 years UIKit experience"],
+            tags: ["SwiftUI", "CoreML", "Vision"],
+            startDate: Date.now,
+            endDate: Date.now.addingTimeInterval(10),
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            milestones: [
+                MockMilestones.milestones[0],
+                MockMilestones.milestones[1]
+            ]
+        ),
     ]
     
     static func addProjects() -> [Project] {
