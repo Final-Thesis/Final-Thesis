38508a59db2e4793a03574315fe14debf5ccf92d
Kevin Dallian <kevindallian@gmail.com>
2023-10-08 16:50:18 +0700

    fixed bug where crash occur when adding profile background in EditView

--- IColab/Features/LoginRegister/ViewModels/LoginViewModel.swift
+++ IColab/Features/LoginRegister/ViewModels/LoginViewModel.swift
@@ -12,10 +12,10 @@ class LoginViewModel: ObservableObject {
   @Published var password = ""
 
   func login() {
-    if let account = MockAccounts.accounts.first(where: { $0.email == email && $0.password == password }) {
-
-    } else {
-    
-    }
+//    if let account = MockAccounts.accounts.first(where: { $0.email == email && $0.password == password }) {
+//
+//    } else {
+//    
+//    }
   }
 }
